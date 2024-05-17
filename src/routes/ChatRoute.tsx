048dfaf256a628f4b662d592ed702761f7816c1e
Eike Lurz <eike.lurz@sportalliance.com>
2024-05-17 19:58:28 +0200

    Remove old gpt version

--- src/routes/ChatRoute.tsx
+++ src/routes/ChatRoute.tsx
@@ -274,10 +274,9 @@ export function ChatRoute() {
                 },
               })}
               data={[
-                { label: 'GPT-3.5', value: 'gpt-3.5-turbo' },
-                { label: 'GPT-4', value: 'gpt-4' }
+                { label: 'GPT-4o', value: 'gpt-4o' }
               ]}
-              onChange={async (value: 'gpt-3.5-turbo' | 'gpt-4') => {
+              onChange={async (value: 'gpt-4o') => {
                 const model = value;
                 try {
                   await db.settings.update("general", {
